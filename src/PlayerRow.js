41048b764def87569a2b857235dd947fb6ad93e2
Quoc PHAN <phquoc25@gmail.com>
2023-03-06 22:51:39 +0700

    use scss for the player row component

--- src/PlayerRow.js
+++ src/PlayerRow.js
@@ -1,11 +1,11 @@
-import './PlayerRow.css';
+import './PlayerRow.scss';
 
 function PlayerRow({ player }) {
     return (
-        <div class="row-container">
-            <span class="name-panel">{player.name}</span>
-            <input type="checkbox" class="switch_1"></input>
-            <input type="checkbox" class="switch_1"></input>
+        <div class="row">
+            <span class="row__label">{player.name}</span>
+            <input class="row__switch" type="checkbox"></input>
+            <input class="row__switch" type="checkbox"></input>
         </div>
     );
 }
